4cbeef371bf2fb3e9e353b8ca2b4b153935fca9b
IgorLevch <igorlevvv@gmail.com>
2024-04-25 05:20:32 +0300

    Addition

---
+++ src/test/java/ru/geekbraines/spring/winter/market/WinterMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbraines.spring.winter.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
